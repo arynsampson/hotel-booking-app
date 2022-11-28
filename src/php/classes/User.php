52c4e42ae0ff43d458b26b5087b5f8f14b4ef164
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2022-11-28 21:12:27 +0200

    user class

---
+++ src/php/classes/User.php
@@ -0,0 +1,38 @@
+<?php
+
+    class User {
+        private $firstname;
+        private $lastname;
+        private $email;
+        private $password;
+
+        public function __construct($firstname, $lastname, $email, $password) {
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        public function getName() {
+            echo $this->firstname . $this->lastname;
+        }
+
+        public function getEmail() {
+            echo $this-email;
+        }
+
+        public function setFirstname($firstname) {
+            $this->firstname = $firstname;
+        }
+
+        public function setLastname($lastname) {
+            $this->lastname = $lastname;
+        }
+
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+
+    }
+
+?>
