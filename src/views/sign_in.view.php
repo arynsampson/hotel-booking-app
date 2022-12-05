25da28a8507c451864366aa76663e04f4ccc6ae4
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2022-12-05 20:59:39 +0200

    added container to sign in page

--- src/views/sign_in.view.php
+++ src/views/sign_in.view.php
@@ -23,40 +23,42 @@ require '../validations/input_validator.php';
     <!-- CSS only -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../style/style.css"> 
     <title>Hotel Booking App</title>
 </head>
 <body>
 
-    <?php require '../../src/templates/header.php'; ?>
+    <?php require '../../src/templates/header.template.php'; ?>
 
     <div class="bg-image">
       <h1>Sign in</h1>
-      <div class="card">
-        <div class="row">
-          <div class="col s12 center-align">
-            <div class="row">
-              <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="col s12">
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input id="email" type="email" class="validate" name="email" required>
-                    <label for="email">Email:</label>
+      <div class="main-container">
+        <div class="card">
+          <div class="row">
+            <div class="col s12 center-align">
+              <div class="row">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="col s12">
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="email" type="email" class="validate" name="email" required>
+                      <label for="email">Email:</label>
+                    </div>
+                    <div class="input-field col s12">
+                      <input id="password" type="password" class="validate" name="password" required>
+                      <label for="password">Password:</label>
+                    </div>
                   </div>
-                  <div class="input-field col s12">
-                    <input id="password" type="password" class="validate" name="password" required>
-                    <label for="password">Password:</label>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="submit" type="submit" class="btn" name="submit" value="Sign in">
+                    </div>
                   </div>
-                </div>
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input id="submit" type="submit" class="btn" name="submit" value="Sign in">
-                  </div>
-                </div>
-                <p><?php echo $_SESSION['error'] ?? '' ; ?></p>
-              </form>
+                  <p><?php echo $_SESSION['error'] ?? '' ; ?></p>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </div> 
       </div>
     </div>
     
