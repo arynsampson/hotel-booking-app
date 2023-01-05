b6c576f9eff3a664330969b182892d992c47e74e
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-05 21:04:36 +0200

    storing fetched hotel in session

--- config/query/fetchHotel.php
+++ config/query/fetchHotel.php
@@ -1,12 +1,11 @@
 <?php
 
-    // require '../../config/connect.php';
-
     function fetchHotel($id) {
         global $conn;
         $sql = "SELECT * FROM hotel where id='$id';";
         $result = $conn->query($sql);
         $hotel = $result->fetch_assoc();
+        $_SESSION['hotel'] = $hotel;
 
         return $hotel;
     }
