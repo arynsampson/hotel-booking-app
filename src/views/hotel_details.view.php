b7782bebcd38467576adc07152059936acc7eae0
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-06 20:52:25 +0200

    hotel details view starter page

--- src/views/hotel_details.view.php
+++ src/views/hotel_details.view.php
@@ -1,14 +1,28 @@
  <?php
 
     session_start();
+    
     require '../../config/connect.php';
     require '../../config/query/fetchHotel.php';
 
+    if($_SESSION['isLoggedIn'] === false) {
+        header('Location: /hotel-booking-app/src/views/sign_in.view.php');
+    }
+
+    
     $url = 'localhost'.$_SERVER['REQUEST_URI'];
     $res = parse_url($url);
     parse_str($res['query'], $params);
-
+    
     $hotel = fetchHotel($params['id']);
+
+    print_r($_SESSION);
+
+        
+    if(isset($_POST['booking-hotel'])) {
+        $_SESSION['booking-information'] = $_POST;
+        header('Location: /hotel-booking-app/src/views/confirm_booking.view.php');
+    }
 
 ?> 
 
@@ -27,16 +41,45 @@
     
     <?php require '../templates/header.template.php'; ?>
 
-    <div style="padding: 10px 30px">
-    <?php
+    <div class="main-container">
+        <div class="hotel-details-card">
+    
+            <h2 class="hotel-name"><?php echo $hotel['name']; ?></h2>
+            <img src="<?php echo $hotel['thumbnail']; ?>" alt="hotel-img" class="hotel-img">
 
-            echo "<h2>$hotel[name]</h2>" . '<br>';
-            echo "<p>R$hotel[daily_rate]</p>" . '<br>';
-            echo "<img src=\"$hotel[thumbnail]\">" . '<br>';
-            echo "<p>Rating: $hotel[rating]</p>" . '<br>';
-            echo "<p>$hotel[address]</p>" . '<br>';
-            ?>
+            <div class="hotel-info-wrapper">
+                <div>
+                    <p class="hotel-info">Price per night: R<?php echo $hotel['daily_rate']; ?></p>
+                    <p class="hotel-info">Rating: <?php echo $hotel['rating']; ?></p>
+                    <p class="hotel-info">Address: <?php echo $hotel['address']; ?></p>
+                </div>
+
+                <div class="hotel-features">
+                    <p class="hotel-feature">Wi-fi</p>
+                    <p class="hotel-feature">Pool</p>
+                    <p class="hotel-feature">Spa</p>
+                    <p class="hotel-feature">Free breakfast</p>
+                    <p class="hotel-feature">Bar</p>
+                </div>
+            </div>
+
+            <div class="hotel-dates">
+                <form action="hotel_details.view.php" method="POST" class="dates-form">
+                    <div class="check-in-wrapper">
+                        <label for="check-in">Check-in date:</label>
+                        <input type="date" name="check-in">
+                    </div>
+                    <div class="check-out-wrapper">
+                        <label for="check-in">Check-out date:</label>
+                        <input type="date" name="check-out">
+                    </div>
+
+                    <input type="submit" value="Book" name="booking-hotel">
+                </form>
+            </div>
+        
+        </div>
     </div>
-  
+    
 </body>
 </html>
