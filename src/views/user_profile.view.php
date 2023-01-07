6d02a88beff558c9e5b346524670aee391bcf42b
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-07 21:15:43 +0200

    added user profile page
    user is able to update certain details about their account here

--- src/views/user_profile.view.php
+++ src/views/user_profile.view.php
@@ -1,5 +1,27 @@
 <?php
     session_start();
+    require '../../config/connect.php';
+    require '../../config/query/fetchUser.php';
+
+    if(isset($_POST['update'])) {
+        $firstname = $_POST['firstname_update'];
+        $lastname = $_POST['lastname_update'];
+        $email = $_POST['email_update'];
+
+        // update user object
+
+
+        // update database
+        global $conn;
+        $sql = "UPDATE user SET firstname='$firstname', lastname='$lastname', email='$email' WHERE user.id='".$_SESSION['loggedInUser']['id']."'";
+        $result = $conn->query($sql);
+
+        // update session variable
+        fetchUser($_SESSION['loggedInUser']['id']);
+
+        header('Location: /hotel-booking-app/src/views/user_profile.view.php');
+    }
+
 
 ?>
 
@@ -9,13 +31,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link rel="stylesheet" href="../style/style.css">
-    <title>Hotel Booking App</title>
+    <title>Hotel Booking</title>
 </head>
 <body>
 
     <?php require '../../src/templates/header.template.php'; ?>
 
-    
+    <div class="main-container">
+        <div class="update-info-form-wrapper">
+            <form action="user_profile.view.php" method="POST">
+                <div>
+                    <label for="firstname">Edit name:</label>
+                    <input type="text" name="firstname_update" value="<?php echo $_SESSION['loggedInUser']['firstname']; ?>">
+                </div>
+                <div>
+                    <label for="lastname">Edit lastname:</label>
+                    <input type="text" name="lastname_update" value="<?php echo $_SESSION['loggedInUser']['lastname']; ?>">
+                </div>
+                <div>
+                    <label for="lastname">Edit email:</label>
+                    <input type="text" name="email_update" value="<?php echo $_SESSION['loggedInUser']['email']; ?>">
+                </div>
+                
+                <input type="submit" value="Update" name="update">
+            </form>
+        </div>
+    </div>
+
 </body>
 </html>
