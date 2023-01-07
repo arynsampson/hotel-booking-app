8d75395efd60ab2a14d40cbadf9a21e8cb724dc2
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-07 21:13:52 +0200

    added bookings page view

---
+++ src/views/bookings.view.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Hotel Booking</title>
+</head>
+<body>
+
+    <?php require '../../src/templates/header.template.php'; ?>
+
+    
+</body>
+</html>
