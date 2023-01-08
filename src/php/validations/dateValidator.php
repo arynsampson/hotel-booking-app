9438def8fde6e2bd6b40df28d4f65aae4add189d
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-08 19:58:27 +0200

    validate user entered dates

--- src/php/validations/dateValidator.php
+++ src/php/validations/dateValidator.php
@@ -3,17 +3,26 @@
     // Validate user date input
     function validateDates($date1, $date2) {
 
+        $hotel_dates = array(
+            ['check-in' => '', 'check-out' => '']
+        );
+
         if(empty($date1) | empty($date2)) {
-            return 'Please enter both dates.';
-        } else {
-            if(date("Y-m-d") > $date1) {
-                return 'Check-in date cannot be before today.';
-            } else {
-                if($date1 > $date2) {
-                    return 'Check-in date should be before check-out date.';
-                } else {
-                    return array($date1, $date2);
-                }
-            }
+            $hotel_dates[1][0] = 'Please enter both dates.';
         }
+        
+        if(date("Y-m-d") > $date1) {
+            $hotel_dates[1][1] = 'Check-in date cannot be before today.';
+        } 
+        
+        if($date1 > $date2) {
+            $hotel_dates[1][2] = 'Check-in date should be before check-out date.';
+        }
+
+        if(!$hotel_dates[1]) {
+            $hotel_dates[0]['check-in'] = $date1;
+            $hotel_dates[0]['check-out'] = $date2;
+        }
+
+        return $hotel_dates;
     }
