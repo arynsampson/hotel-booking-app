8948977fdafc0581faec455e7ebdf7dcd1cbb26e
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-08 19:58:58 +0200

    added dynamic data

--- src/views/confirm_booking.view.php
+++ src/views/confirm_booking.view.php
@@ -6,12 +6,12 @@
     require '../../config/connect.php';
 
     $booking_info = array(
-        "hotel_name" => "Grand Kloof Hotel",
+        "hotel_name" => $_SESSION['hotel']['name'],
         "num_rooms" => 2,
         "num_nights" => 3,
         "daily_rate" => 2700,
         "rating" => 5,
-        "total_cost" => 16200
+        "total_cost" => $booking_info['total_cost']
     );
 
     if(isset($_POST['confirm-booking'])) {
