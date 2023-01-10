ad632d09962d8ca82a54e55461ab764295165307
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-10 22:46:48 +0200

    created utils class
    - contains validation functions
    - contains utility functions

---
+++ src/php/classes/Utils.class.php
@@ -0,0 +1,116 @@
+<?php
+
+    include_once 'DB.class.php';
+
+    class Utils extends DB {
+        
+        // get the difference between two dates
+        // takes 2 dates as params, returns the difference as a number
+        public function dateDifference($date1, $date2) {
+            $dateDifferenceAmount = date_diff(date_create($date1), date_create($date2));
+            return $dateDifferenceAmount->format("%a");
+        }
+
+        // calculate the total cost of the user stay
+        public function totalStayCost($numOfDays, $dailyRate) {
+            return $numOfDays * $dailyRate;
+        }
+
+        // Validate user date input
+        public function validateDates($date1, $date2) {
+            $hotel_dates = array(
+                ['check-in' => '', 'check-out' => '']
+            );
+            if(empty($date1) | empty($date2)) {
+                $hotel_dates[1][0] = 'Please enter both dates.';
+            } else {
+                $hotel_dates[1][0] = '';
+                if(date("Y-m-d") > $date1) {
+                    $hotel_dates[1][1] = 'Check-in date cannot be before today.';
+                } else {
+                    if($date1 > $date2) {
+                        $hotel_dates[1][2] = 'Check-out date should be after check-in date.';
+                    } else {
+                        if(!$hotel_dates[1]) {
+                            $hotel_dates[0]['check-in'] = $date1;
+                            $hotel_dates[0]['check-out'] = $date2;
+                        }
+                    }
+                }
+            }
+            return $hotel_dates;
+        }
+
+        // validate user email
+        public function validate_email($email) {
+            if(strlen($email) < 2) {
+                return array(
+                    'email' => $email, 
+                    'error' => 'Cannot be less than 2 characters.'
+                );
+            } else {
+                $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+                return $email;
+            }
+        }
+
+        // validate user password
+        public function validate_password($password) {
+            if(strlen($password) < 5) {
+                return array(
+                    'email' => '', 
+                    'error' => 'Cannot be less than 5 characters.'
+                );
+            } else {
+                return $password;
+            }
+        }
+
+        // validate user firstname input
+        public function validate_firstname($input) {
+            if(strlen($input) < 2) {
+              return array(
+                "firstname" => $input,
+                "error" => "Cannot be less than 2 characters."
+              );
+            } else {
+              $input = trim($input);
+              $input = stripslashes($input);
+              $input = htmlspecialchars($input);
+              return $input;
+            }
+        }
+
+        // validate user lastname input
+        public function validate_lastname($input) {
+            if(strlen($input) < 2) {
+              return array(
+                "lastname" => $input,
+                "error" => "Cannot be less than 2 characters."
+              );
+            } else {
+              $input = trim($input);
+              $input = stripslashes($input);
+              $input = htmlspecialchars($input);
+              return $input;
+            }
+        }
+
+        // check if user exists in db
+        public function checkUserExists($email) {
+            $db = new DB;
+            $conn = $db->getConn();
+
+            $sql = "SELECT * FROM user WHERE user.email = '$email'";
+            $result = $conn->query($sql);
+
+            if($result->num_rows < 1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        
+    }
+    
+?>
