907ab0abb072eb564cca8076bf201c702db23495
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-10 22:48:04 +0200

    temporary duplicate of user input validations

--- src/php/validations/input_validator.php
+++ src/php/validations/input_validator.php
@@ -1,15 +1,30 @@
 <?php
 
   function validate_input($input) {
-    $input = trim($input);
-    $input = stripslashes($input);
-    $input = htmlspecialchars($input);
-    return $input;
+    if(strlen($input) < 2) {
+      return array(
+        "name" => $input,
+        "error" => "Cannot be less than 2 characters."
+      );
+    } else {
+      $input = trim($input);
+      $input = stripslashes($input);
+      $input = htmlspecialchars($input);
+      return array('name' => $input);
+    }
   }
 
+  // validate user email
   function validate_email($email) {
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    return $email;
+    if(strlen($email) < 2) {
+      return array(
+        'email' => $email, 
+        'error' => 'Cannot be less than 2 characters.'
+      );
+    } else {
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+      return array('email' => $email);
+    }
   }
-  
+
 ?>
