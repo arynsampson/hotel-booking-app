c1cc42a24b86343213f278e55669a4841fa5db6c
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-10 22:45:52 +0200

    created database class with db connection

---
+++ src/php/classes/DB.class.php
@@ -0,0 +1,19 @@
+<?php
+
+    require '../../../config/connection_details.php';
+
+    class DB {
+        private $server = 'localhost';
+        private $username = 'admin';
+        private $password = 'password1234';
+        private $db = 'hotel';
+        public $conn;
+
+        public function __construct() {
+            $this->conn = new mysqli($this->server, $this->username, $this->password, $this->db);
+        }
+
+        public function getConn() {
+            return $this->conn;
+        }
+    }
