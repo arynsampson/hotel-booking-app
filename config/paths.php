6093657f7ef7a7ec92f19da41799aa0668f261ac
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-11 07:19:01 +0200

    created file to store all file paths

---
+++ config/paths.php
@@ -0,0 +1,8 @@
+<?php
+
+    $homePage = '/hotel-booking-app/index.php';
+    $profilePage = '/hotel-booking-app/src/views/user_profile.view.php';
+    $bookingsPage = '/hotel-booking-app/src/views/bookings.view.php';
+    $signInPage = '/hotel-booking-app/src/views/sign_in.view.php';
+    $signUpPage = '/hotel-booking-app/src/views/register.view.php';
+    $logout = '/hotel-booking-app/src/php/user_handling/logout_handler.php';
