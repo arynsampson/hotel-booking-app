447910de4b3b2b954614d16707659a9233ff2a86
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-11 07:19:51 +0200

    removed hard coded file paths from tmpl

--- src/templates/header.template.php
+++ src/templates/header.template.php
@@ -1,12 +1,7 @@
 <?php
 
+    require '../../config/paths.php';
     $navOptions = [];
-    $homePage = '/hotel-booking-app/index.php';
-    $profilePage = '/hotel-booking-app/src/views/user_profile.view.php';
-    $bookingsPage = '/hotel-booking-app/src/views/bookings.view.php';
-    $signInPage = '/hotel-booking-app/src/views/sign_in.view.php';
-    $signUpPage = '/hotel-booking-app/src/views/register.view.php';
-    $logout = '/hotel-booking-app/src/php/user_handling/logout_handler.php';
 
     if($_SESSION['isLoggedIn']) {
         $navOptions = [
