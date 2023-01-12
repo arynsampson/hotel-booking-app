c7ecc148a497f5625dae81f93cf11703a827ba37
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-12 20:56:11 +0200

    removed code from view file

--- src/views/sign_in.view.php
+++ src/views/sign_in.view.php
@@ -1,20 +1,3 @@
-<?php
-
-require '../php/user_handling/sign_in_handler.php';
-require '../php/classes/Utils.php';
-
-  if(isset($_POST['submit'])) {
-    $utils = new Utils;
-    $user = array(
-      'email' => $utils->validate_email($_POST['email']),
-      'password' => $utils->validate_password($_POST['password'])
-    );
-
-    signUserIn($user);
-  }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,7 +12,7 @@ require '../php/classes/Utils.php';
 </head>
 <body>
 
-    <?php require '../../src/templates/header.template.php'; ?>
+    <?php session_start(); require '../../src/templates/header.template.php'; ?>
 
     <div class="bg-image">
       <h1>Sign in</h1>
@@ -38,7 +21,7 @@ require '../php/classes/Utils.php';
           <div class="row">
             <div class="col s12 center-align">
               <div class="row">
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="col s12">
+                <form action="<?php echo '../php/user_handling/sign_in_handler.php'; ?>" method="POST" class="col s12">
                   <div class="row">
                     <div class="input-field col s12">
                       <input id="email" type="email" class="validate" name="email" required>
