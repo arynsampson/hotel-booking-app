d1843934fbafaaecedbc8251a0a9a6f05673f72d
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-14 13:04:42 +0200

    add bookings date check for cancellation
    user cannot cancel a booking when it is within 48 hours from current date

--- src/php/booking_handling/cancel.php
+++ src/php/booking_handling/cancel.php
@@ -1,15 +1,25 @@
 <?php
 
+    require '../../../config/connect.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/hotel-booking-app/config/query/fetchBooking.php';
+    require_once $_SERVER['DOCUMENT_ROOT'].'/hotel-booking-app/src/php/classes/Utils.class.php';
     session_start();
 
-    require '../../../config/connect.php';
+    $utils = new Utils;
 
-    $url = 'localhost'.$_SERVER['REQUEST_URI'];
-    $res = parse_url($url);
-    parse_str($res['query'], $params);
-    $booking_id = $params['id'];
+    $bookingID = $_GET['id'];
+    $booking = fetchBooking($bookingID);
+    $currentDate = date('Y-m-d');
+    $datesDifference = $utils->dateDifference($currentDate, $booking['check_in_date']);
     
-    $sql = "UPDATE booking SET status='CANCELLED' WHERE booking.id='$booking_id'";
-    $result = $conn->query($sql);
+    if($datesDifference > 2) {
+        $sql = "UPDATE booking SET status='CANCELLED' WHERE booking.id='$bookingID'";
+        $result = $conn->query($sql);
+    } else {
+        // TODO: add error message to bookings view
+        $_SESSION['cancel_error'] = 'Bookings with check-in dates within 48 hours cannot be cancelled.';
+    }
 
     header('Location: /hotel-booking-app/src/views/bookings.view.php');
+
+?>
