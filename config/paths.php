b03c70e4685a2780c9d468c0a4629ae687cb5e87
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-22 22:44:30 +0200

    update file paths to match new directories

--- config/paths.php
+++ config/paths.php
@@ -1,11 +1,11 @@
 <?php
 
     $homePage = '/hotel-booking-app/index.php';
-    $profilePage = '/hotel-booking-app/src/views/user_profile.view.php';
+    $profilePage = '/hotel-booking-app/src/views/userProfile.view.php';
     $bookingsPage = '/hotel-booking-app/src/views/bookings.view.php';
-    $signInPage = '/hotel-booking-app/src/views/sign_in.view.php';
-    $signUpPage = '/hotel-booking-app/src/views/register.view.php';
-    $logout = '/hotel-booking-app/src/php/user_handling/logout_handler.php';
+    $login = '/hotel-booking-app/src/views/login.view.php';
+    $register = '/hotel-booking-app/src/views/register.view.php';
+    $logout = '/hotel-booking-app/src/php/handling/user_handling/AuthHandler.php';
     $userClass_path = '/hotel-booking-app/src/php/classes/User.class.php';  
 
 ?>
