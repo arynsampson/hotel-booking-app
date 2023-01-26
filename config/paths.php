9be47f1b70c5f414c5ec9b3753ee060a3ef98c7d
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2023-01-26 21:00:02 +0200

    updated logout page file path

--- config/paths.php
+++ config/paths.php
@@ -5,7 +5,7 @@
     $bookingsPage = '/hotel-booking-app/src/views/bookings.view.php';
     $login = '/hotel-booking-app/src/views/login.view.php';
     $register = '/hotel-booking-app/src/views/register.view.php';
-    $logout = '/hotel-booking-app/src/php/handling/user_handling/AuthHandler.php';
+    $logout = '/hotel-booking-app/src/php/handling/auth/AuthHandler.php';
     $userClass_path = '/hotel-booking-app/src/php/classes/User.class.php';  
 
 ?>
